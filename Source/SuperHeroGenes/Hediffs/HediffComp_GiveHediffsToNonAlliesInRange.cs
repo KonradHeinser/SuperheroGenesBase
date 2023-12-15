76eb372d39cebe164d61fa6b2d6eab5005fad40e
Robert <robhos2000@gmail.com>
2023-12-15 13:57:38 -0500

    Debuffer fix

--- Source/SuperHeroGenes/Hediffs/HediffComp_GiveHediffsToNonAlliesInRange.cs
+++ Source/SuperHeroGenes/Hediffs/HediffComp_GiveHediffsToNonAlliesInRange.cs
@@ -36,10 +36,8 @@ namespace SuperHeroGenesBase
             {
                 foreach (Pawn item in list)
                 {
-                    if (item.Dead || item.health == null || (Props.targetingParameters != null && !Props.targetingParameters.CanTarget(item)) || allies.Contains(item))
-                    {
-                        continue;
-                    }
+                    if (allies.Contains(item) || (item.Faction != null && item.Faction.AllyOrNeutralTo(Pawn.Faction))) continue; // If it's an ally/non-enemy
+                    if (item.Dead || item.health == null || (Props.targetingParameters != null && !Props.targetingParameters.CanTarget(item))) continue;
                     Log.Message("Checking range");
                     if (Props.rangeStat != null)
                     {
