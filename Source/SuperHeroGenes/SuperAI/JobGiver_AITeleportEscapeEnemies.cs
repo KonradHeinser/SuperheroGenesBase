b6b18fd01b0e114d48feeb26b44d7ab3dba8d9fd
Robert <robhos2000@gmail.com>
2023-12-20 21:40:13 -0500

    Added automatic fleeing

---
+++ Source/SuperHeroGenes/SuperAI/JobGiver_AITeleportEscapeEnemies.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Verse;
+using Verse.AI;
+using RimWorld;
+
+namespace SuperHeroGenesBase
+{
+    public class JobGiver_AITeleportEscapeEnemies : ThinkNode_JobGiver
+    {
+        private AbilityDef ability;
+
+        private static List<Thing> tmpHostileSpots = new List<Thing>();
+
+        protected override Job TryGiveJob(Pawn pawn)
+        {
+            Ability ability = pawn.abilities?.GetAbility(this.ability);
+            if (ability == null || !ability.CanCast || ability.comps.NullOrEmpty())
+            {
+                return null;
+            }
+            float range = 0f;
+            foreach (AbilityComp comp in ability.comps)
+            {
+                if (comp is CompAbilityEffect_Teleport compAbilityEffect_Teleport)
+                {
+                    if (compAbilityEffect_Teleport.Props.range > 0) range = compAbilityEffect_Teleport.Props.range;
+                    else range = compAbilityEffect_Teleport.Props.randomRange.RandomInRange;
+                    break;
+                }
+            }
+            if (TryFindRelocatePosition(pawn, out var relocatePosition, 1f))
+            {
+                return ability.GetJob(pawn, relocatePosition);
+            }
+            return null;
+        }
+
+        private bool TryFindRelocatePosition(Pawn pawn, out IntVec3 relocatePosition, float maxDistance)
+        {
+            tmpHostileSpots.Clear();
+            tmpHostileSpots.AddRange(from a in pawn.Map.attackTargetsCache.GetPotentialTargetsFor(pawn)
+                                     where !a.ThreatDisabled(pawn)
+                                     select a.Thing);
+            Ability jump = pawn.abilities?.GetAbility(ability);
+            relocatePosition = CellFinderLoose.GetFallbackDest(pawn, tmpHostileSpots, maxDistance, 5f, 5f, 20, (IntVec3 c) => jump.verb.ValidateTarget(c, showMessages: false));
+            tmpHostileSpots.Clear();
+            return relocatePosition.IsValid;
+        }
+
+        public override ThinkNode DeepCopy(bool resolve = true)
+        {
+            JobGiver_AITeleportEscapeEnemies obj = (JobGiver_AITeleportEscapeEnemies)base.DeepCopy(resolve);
+            obj.ability = ability;
+            return obj;
+        }
+    }
+}
---
+++ Source/SuperHeroGenes/SuperAI/ThinkNode_ConditionalColonistShouldFlee.cs
@@ -0,0 +1,73 @@
+﻿using Verse;
+using Verse.AI;
+using Verse.AI.Group;
+using RimWorld;
+
+namespace SuperHeroGenesBase
+{
+    public class ThinkNode_ConditionalColonistShouldFlee : ThinkNode_Conditional
+    {
+        protected override bool Satisfied(Pawn pawn)
+        {
+            Pawn meleeThreat = pawn.mindState.meleeThreat;
+
+            if (meleeThreat == null)
+            {
+                return false;
+            }
+            if (meleeThreat.IsInvisible() || pawn.IsInvisible())
+            {
+                return false;
+            }
+            if (IsHunting(pawn, meleeThreat))
+            {
+                return false;
+            }
+            if (IsDueling(pawn, meleeThreat))
+            {
+                return false;
+            }
+            if (PawnUtility.PlayerForcedJobNowOrSoon(pawn))
+            {
+                return false;
+            }
+            if (!pawn.mindState.MeleeThreatStillThreat)
+            {
+                pawn.mindState.meleeThreat = null;
+                return false;
+            }
+            if (pawn.playerSettings != null && pawn.playerSettings.UsesConfigurableHostilityResponse && pawn.playerSettings.hostilityResponse == HostilityResponseMode.Flee)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool IsDueling(Pawn pawn, Pawn other)
+        {
+            if (pawn.GetLord()?.LordJob is LordJob_Ritual_Duel lordJob_Ritual_Duel)
+            {
+                return lordJob_Ritual_Duel.Opponent(pawn) == other;
+            }
+            return false;
+        }
+
+        private bool IsHunting(Pawn pawn, Pawn prey)
+        {
+            if (pawn.CurJob == null)
+            {
+                return false;
+            }
+            if (pawn.jobs.curDriver is JobDriver_Hunt jobDriver_Hunt)
+            {
+                return jobDriver_Hunt.Victim == prey;
+            }
+            if (pawn.jobs.curDriver is JobDriver_PredatorHunt jobDriver_PredatorHunt)
+            {
+                return jobDriver_PredatorHunt.Prey == prey;
+            }
+            return false;
+        }
+    }
+}
