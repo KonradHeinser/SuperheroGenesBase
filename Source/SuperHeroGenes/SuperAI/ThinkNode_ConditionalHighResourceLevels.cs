60656225b50106b07aba2f9de500fab4399acca2
Robert <robhos2000@gmail.com>
2023-12-26 08:19:05 -0500

    Autocast buffs complete

--- Source/SuperHeroGenes/SuperAI/ThinkNode_ConditionalHighResourceLevels.cs
+++ Source/SuperHeroGenes/SuperAI/ThinkNode_ConditionalHighResourceLevels.cs
@@ -7,6 +7,7 @@ namespace SuperHeroGenesBase
     public class ThinkNode_ConditionalHighResourceLevels : ThinkNode_Conditional
     {
         private float minLevel = 0.9f;
+        private bool useTargetValue = true;
         private GeneDef gene = null;
         protected override bool Satisfied(Pawn pawn)
         {
@@ -14,6 +15,7 @@ namespace SuperHeroGenesBase
             try
             {
                 Gene_Resource resourceGene = (Gene_Resource)pawn.genes.GetGene(gene);
+                if (useTargetValue) return resourceGene.Value >= resourceGene.targetValue;
                 return resourceGene.Value >= minLevel;
             }
             catch
