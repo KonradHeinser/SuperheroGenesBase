9b9d4bcda9761254950e3603604f9ea7e933c1bd
Robert <robhos2000@gmail.com>
2024-05-03 09:46:24 -0400

    Bug fix and added more immunities to expelled radiation

--- Source/SuperHeroGenes/Hediffs/CompProperties/HediffCompProperties_ExplodeWhenDamaged.cs
+++ Source/SuperHeroGenes/Hediffs/CompProperties/HediffCompProperties_ExplodeWhenDamaged.cs
@@ -3,9 +3,11 @@ namespace SuperHeroGenesBase
 {
     public class HediffCompProperties_ExplodeWhenDamaged : BurstHediffPropertiesBase
     {
+        public int cooldownTicks = 60; // Set to once every other second to ensurethe first explosion is done before starting the next one
+
         public HediffCompProperties_ExplodeWhenDamaged()
         {
-            compClass = typeof(HediffComp_ExplodeOnDamaged);
+            compClass = typeof(HediffComp_ExplodeWhenDamaged);
         }
     }
 }
--- Source/SuperHeroGenes/Hediffs/Comps/HediffComp_ExplodeOnDamaged.cs
+++ Source/SuperHeroGenes/Hediffs/Comps/HediffComp_ExplodeOnDamaged.cs
@@ -1,14 +1,24 @@
 ﻿using Verse;
 namespace SuperHeroGenesBase
 {
-    public class HediffComp_ExplodeOnDamaged : BurstHediffCompBase
+    public class HediffComp_ExplodeWhenDamaged : BurstHediffCompBase
     {
         public new HediffCompProperties_ExplodeWhenDamaged Props => (HediffCompProperties_ExplodeWhenDamaged)props;
 
+        public int cooldownTicks = 0; // Not saved because this is just to avoid performance issues
+
+        public override void CompPostTick(ref float severityAdjustment)
+        {
+            if (cooldownTicks > 0) cooldownTicks--;
+        }
+
         public override void Notify_PawnPostApplyDamage(DamageInfo dinfo, float totalDamageDealt)
         {
-            if (parent.Severity >= Props.minSeverity)
+            if (cooldownTicks <= 0 && parent.Severity >= Props.minSeverity && parent.Severity < Props.maxSeverity)
+            {
                 DoExplosion(parent.pawn.Position);
+                cooldownTicks = Props.cooldownTicks;
+            }
         }
     }
 }
