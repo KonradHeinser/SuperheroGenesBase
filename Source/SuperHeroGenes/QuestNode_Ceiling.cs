9844f0e4c04dff20ded107b32379fd8751757870
Robert <robhos2000@gmail.com>
2024-08-17 20:34:02 -0400

    Made ceiling quest node

---
+++ Source/SuperHeroGenes/QuestNode_Ceiling.cs
@@ -0,0 +1,25 @@
+﻿using RimWorld.QuestGen;
+using System;
+using Verse;
+
+namespace SuperHeroGenesBase
+{
+    public class QuestNode_Ceiling : QuestNode
+    {
+        public SlateRef<double> value;
+
+        [NoTranslate]
+        public SlateRef<string> storeAs;
+
+        protected override bool TestRunInt(Slate slate)
+        {
+            return !storeAs.GetValue(slate).NullOrEmpty();
+        }
+
+        protected override void RunInt()
+        {
+            Slate slate = QuestGen.slate;
+            slate.Set(storeAs.GetValue(slate), Math.Ceiling(value.GetValue(slate)));
+        }
+    }
+}
