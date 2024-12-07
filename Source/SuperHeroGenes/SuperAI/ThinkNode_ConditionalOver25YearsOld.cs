092e54f266c7a1954fd2648cd7f3020dedb9b97f
Robert <robhos2000@gmail.com>
2024-12-07 17:20:12 -0500

    Added hemomancer and radiomancer pills

---
+++ Source/SuperHeroGenes/SuperAI/ThinkNode_ConditionalOver25YearsOld.cs
@@ -0,0 +1,13 @@
+﻿using Verse;
+using Verse.AI;
+
+namespace SuperHeroGenesBase
+{
+    public class ThinkNode_ConditionalOver25YearsOld : ThinkNode_Conditional
+    {
+        protected override bool Satisfied(Pawn pawn)
+        {
+            return pawn.ageTracker.AgeBiologicalYearsFloat > 25f;
+        }
+    }
+}
