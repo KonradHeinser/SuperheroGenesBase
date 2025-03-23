22f7a953c13e30852a5515a5e63587d34266ff74
KonradHeinser <robhos2000@gmail.com>
2025-03-22 19:03:05 -0700

    Porting attach mote

---
+++ Source/SuperHeroGenes/Hediffs/Comps/HediffComp_AttachMote.cs
@@ -0,0 +1,40 @@
+﻿using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace SuperHeroGenesBase
+{
+    public class HediffComp_AttachMote : HediffComp
+    {
+        HediffCompProperties_AttachMote Props => (HediffCompProperties_AttachMote)props;
+
+        private Mote mote;
+
+        private float Brightness
+        {
+            get
+            {
+                if (!Props.brightnessBySeverity) return Props.staticBrightness;
+                return Mathf.Min(parent.Severity, 1);
+            }
+        }
+
+        public override void CompPostTick(ref float severityAdjustment)
+        {
+            base.CompPostTick(ref severityAdjustment);
+            if (Pawn.Spawned && (!Pawn.Downed || Props.displayWhileDowned))
+            {
+                if (mote == null || mote.Destroyed)
+                {
+                    mote = MoteMaker.MakeAttachedOverlay(Pawn, Props.moteDef, Vector3.zero);
+                    mote.link1.rotateWithTarget = Props.rotateWithPawn;
+                }
+
+                if (Props.scaleMoteWithSize)
+                    mote.Scale = Pawn.BodySize;
+                mote.instanceColor = new Color(Props.color.r, Props.color.g, Props.color.b, Brightness);
+                mote.Maintain();
+            }
+        }
+    }
+}
