b886f5e28a132f2df65cfaa5e7b3e481fb306df0
KonradHeinser <robhos2000@gmail.com>
2025-09-27 09:35:07 -0700

    Copy Ideo Quest Node

---
+++ Source/SuperHeroGenes/Quest/QuestNode_CopyIdeology.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using RimWorld;
+using RimWorld.QuestGen;
+using Verse;
+using Verse.AI;
+
+namespace SuperHeroGenesBase
+{
+    public class QuestNode_CopyIdeology : QuestNode
+    {
+        public SlateRef<Pawn> origin;
+
+        public SlateRef<Pawn> target;
+
+        protected override void RunInt()
+        {
+            Slate slate = QuestGen.slate;
+            target.GetValue(slate).ideo.SetIdeo(origin.GetValue(slate)?.Ideo);
+        }
+
+        protected override bool TestRunInt(Slate slate)
+        {
+            if (origin.GetValue(slate)?.Ideo == null)
+                return false;
+
+            if (target.GetValue(slate)?.ideo == null)
+                return false;
+
+            return true;
+        }
+    }
+}
